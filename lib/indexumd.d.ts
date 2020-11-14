3d7a062f4b6d44f7684901cb4c43b5b19bad86e2
Putri Karunia <putri@cotter.app>
2020-11-13 19:18:56 -0800

    Publish beta

--- lib/indexumd.d.ts
+++ lib/indexumd.d.ts
@@ -1,6 +1,6 @@
 import Cotter from "./Cotter";
 import CotterEnum from "./enum";
 declare class CotterExport extends Cotter {
-    CotterEnum: CotterEnum;
+    static CotterEnum: typeof CotterEnum;
 }
 export = CotterExport;
