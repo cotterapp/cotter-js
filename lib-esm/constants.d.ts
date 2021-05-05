7333a55e79089392aebddaf1eb71f809932111ca
Putri Karunia <putri@cotter.app>
2021-05-05 14:25:49 -0700

    Yalc publish

--- lib-esm/constants.d.ts
+++ lib-esm/constants.d.ts
@@ -1,6 +1,7 @@
 import { AUTHENTICATION_METHOD, IDENTIFIER_TYPE } from "./binder";
 export declare const ATTR_API_KEY_ID = "data-cotter-api-key-id";
 export declare const ATTR_ENV = "data-cotter-env";
+export declare const ATTR_CONFIG = "data-cotter-config";
 export declare const DEFAULT_FORM_SETTINGS: {
     authenticationMethod: AUTHENTICATION_METHOD;
     type: IDENTIFIER_TYPE;
--- lib-esm/constants.js
+++ lib-esm/constants.js
@@ -1,6 +1,7 @@
 import { AUTHENTICATION_METHOD, IDENTIFIER_TYPE } from "./binder";
 export var ATTR_API_KEY_ID = "data-cotter-api-key-id";
 export var ATTR_ENV = "data-cotter-env";
+export var ATTR_CONFIG = "data-cotter-config";
 export var DEFAULT_FORM_SETTINGS = {
     authenticationMethod: AUTHENTICATION_METHOD.MAGIC_LINK,
     type: IDENTIFIER_TYPE.EMAIL,
